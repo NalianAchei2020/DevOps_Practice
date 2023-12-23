bd42c5efb40ecc0a283d238e6751f4ee23597be1
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-12-23 05:17:15 +0100

    make changes

--- Services/user authentication/main.py
+++ Services/user authentication/main.py
@@ -1,39 +1,92 @@
+"""
 from fastapi import FastAPI
-from redis_om import get_redis_connection
-from fastapi import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import redis
 
-app = FastAPI();
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['*'],
+    allow_origins=['*'],
     allow_methods=['*'],
     allow_headers=['*']
 )
 
-# connecting to redis database
+# Configure Redis connection
+redis_host = "redis-17977.c267.us-east-1-4.ec2.cloud.redislabs.com"
+redis_port = 17977
+redis_password = "Nalian@2020"
+redis_db = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 
+# Define User model
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+@app.get('/users')
+async def read_users():
+    user_data = redis_db.hgetall("users")
+    users = []
+    for user_dict in user_data.values():
+        user_json = user_dict  # No need for decoding in this case
+        user = User.parse_raw(user_json)  # Parse JSON string to User object
+        users.append(user)
+    return users
+
+@app.post('/users')
+async def create_user(user: User):
+    redis_db.hset("users", user.name, user.json())
+    return {"message": "User created successfully"}
+    """
+    
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# Configure Redis connection
+redis_host = "redis-17977.c267.us-east-1-4.ec2.cloud.redislabs.com"
+redis_port = 17977
+redis_password = "Nalian@2020"
 redis = get_redis_connection(
-    host="redis-17977.c267.us-east-1-4.ec2.cloud.redislabs.com:",
-    port=17977,
-    password = "Nalian@2020",
+    host=redis_host,
+    port=redis_port,
+    password=redis_password,
     decode_responses=True
 )
 
-# creating a database model
-class User(redis.Model):
+# Define User model
+class User(HashModel):
     name: str
     password: str
     
-    
+    # Define Redis-specific fields
+    name = "user"  # Name of the Redis hash
     database = redis
-    
 
 @app.get("/")
-
-async def root():
+async def read_root():
     return {"message": "Hello World"}
 
-@app.get('/use')
-async def all():
+@app.get('/users', methods=['GET'])
+async def read_users():
     return User.all()
+
+@app.post('/users', methods=['POST'])
+async def create_user(user: User):
+    return user.save()
